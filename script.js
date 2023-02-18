07e9eedb2406500ed9dbab894585ebb1d72ff7c3
HiAlfie <66980044+HiAlfie@users.noreply.github.com>
2023-02-18 22:36:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,49 @@
-// Get the necessary HTML elements
-const homeScreen = document.querySelector('.home-screen');
-const nameForm = document.querySelector('form');
-const gameScreen = document.querySelector('.game-screen');
-const generateButton = document.getElementById('generate-button');
-const numberOutput = document.getElementById('number-output');
-const numberIndex = document.getElementById('number-index');
+// get the DOM elements
+const nameInput = document.getElementById("name");
+const submitBtn = document.getElementById("submitBtn");
+const generateBtn = document.getElementById("generateBtn");
+const indexList = document.getElementById("indexList");
 
-// Initialize an empty array to keep track of generated numbers
+// create an array to keep track of generated numbers
 let generatedNumbers = [];
 
-// Function to generate a random number between 1 and 1000
+// function to generate a random number between 1 and 1000
 function generateNumber() {
   let number = Math.floor(Math.random() * 1000) + 1;
-  // If the number has already been generated, generate a new number
-  while (generatedNumbers.includes(number)) {
-    number = Math.floor(Math.random() * 1000) + 1;
-  }
-  // Add the number to the generatedNumbers array
+  return number;
+}
+
+// function to check if a number has already been generated
+function hasGeneratedNumber(number) {
+  return generatedNumbers.includes(number);
+}
+
+// function to mark a number as generated in the index list
+function markAsGenerated(number) {
+  let indexListItem = indexList.children[number - 1];
+  indexListItem.classList.add("generated");
+}
+
+// function to handle the "generate number" button click
+function onGenerateClick() {
+  let number = generateNumber();
   generatedNumbers.push(number);
-  // Update the UI with the generated number and the number index
-  numberOutput.innerText = number;
-  const indexItem = numberIndex.children[number - 1];
-  indexItem.style.backgroundColor = 'green';
-  // If all 1000 numbers have been generated, send a message to the Discord webhook
-  if (generatedNumbers.length === 1000) {
-    const username = localStorage.getItem('username');
-    const webhookUrl = 'https://discord.com/api/webhooks/1076179716337303593/O-o8ISaDgHsupAkcKKzlEjlLYtdSDkwRqb0uqtCDEDJ8nqJtOK5aZh999vUOS9SU7luA';
-    const message = `${username} has completed the game!`;
-    fetch(webhookUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ content: message })
-    });
+  markAsGenerated(number);
+}
+
+// function to handle the "submit" button click
+function onSubmitClick() {
+  const name = nameInput.value.trim();
+  if (name === "") {
+    alert("Please enter your name to start the game.");
+  } else {
+    // store the name in local storage
+    localStorage.setItem("name", name);
+    // switch to the game page
+    window.location.href = "game.html";
   }
 }
 
-// Event listener for the name form submission
-nameForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const nameInput = document.getElementById('name');
-  const name = document.getElementById("nameInput").value.trim();
-  localStorage.setItem('username', name);
-  homeScreen.style.display = 'none';
-  gameScreen.style.display = 'block';
-});
-
-// Event listener for the generate button
-generateButton.addEventListener('click', generateNumber);
-
-// Populate the number index list with 1000 items
-for (let i = 1; i <= 1000; i++) {
-  const item = document.createElement('li');
-  item.innerText = i;
-  numberIndex.appendChild(item);
-}
+// add event listeners
+submitBtn.addEventListener("click", onSubmitClick);
+generateBtn.addEventListener("click", onGenerateClick);
